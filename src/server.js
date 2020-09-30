ab936adee031dc9cd6ff035d98ed648379610f62
Andres Castillo Quesada <>
2020-09-30 07:59:26 -0600

    Working on auth

--- src/server.js
+++ src/server.js
@@ -11,22 +11,22 @@ const { uri } = require('./env/constants')
 //const uri = "mongodb+srv://<username>:<password>@<your-cluster-url>/test?retryWrites=true&w=majority";
 
 async function main() {
-	try {
-		mongoose.Promise = global.Promise
-		mongoose.set('useNewUrlParser', true)
-		mongoose.set('useUnifiedTopology', true)
-		await mongoose.connect(env.uri+env.md+env.db, () => {
-			console.log('La BD funciona '+env.uri+env.md+env.db)
+  try {
+    mongoose.Promise = global.Promise
+    mongoose.set('useNewUrlParser', true)
+    mongoose.set('useUnifiedTopology', true)
+    await mongoose.connect(env.uri + env.md + env.db, () => {
+      console.log('La BD funciona ' + env.uri + env.md + env.db)
 
-			app.listen(env.port, () => {
-				console.log('REST API server '+env.uri+env.port)
-			})
-		})
-	} catch (error) {
-		console.log(error)
-	} finally {
-		// close
-	}
+      app.listen(env.port, () => {
+        console.log('REST API server ' + env.uri + env.port)
+      })
+    })
+  } catch (error) {
+    console.log(error)
+  } finally {
+    // close
+  }
 }
 
 main()
