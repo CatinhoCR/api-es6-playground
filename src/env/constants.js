c930b94f773b161dd4e1940144b6cab7fd325fca
Andres Castillo Quesada <>
2020-10-03 00:10:23 -0600

    Removing old code base in favor of new similar flow

--- src/env/constants.js
+++
@@ -1,6 +0,0 @@
-const uri = 'mongodb://localhost:'
-const md = '27017/'
-const db = 'js_es6_playground'
-const port = process.env.PORT
-
-module.exports = { uri, md, db, port }
--- src/server.js
+++ src/server.js
@@ -1,35 +1,3 @@
-'use strict'
-// const env = require('./env/constants')
+require('dotenv').config()
 const mongoose = require('mongoose')
 const app = require('./app')
-// const { uri } = require('./env/constants')
-const { uri, md, db } = require('./env/constants')
-require('dotenv').config();
-
-/**
- * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
- * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
- */
-//const uri = "mongodb+srv://<username>:<password>@<your-cluster-url>/test?retryWrites=true&w=majority";
-
-async function main() {
-	try {
-		mongoose.Promise = global.Promise
-		mongoose.set('useNewUrlParser', true)
-		mongoose.set('useUnifiedTopology', true)
-		await mongoose.connect(uri+md+db, () => {
-			console.log('La BD funciona '+uri+md+db)
-
-      // env.port
-			app.listen(process.env.PORT, () => {
-        console.log(`REST API server port: ${process.env.PORT}`)
-			})
-		})
-	} catch (error) {
-		console.log(error)
-	} finally {
-		// close
-	}
-}
-
-main()
